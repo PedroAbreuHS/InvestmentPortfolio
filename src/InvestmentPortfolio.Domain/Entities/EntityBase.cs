511046769e26db67d15530016505b4b202aa7b4a
PedroAbreuHS <104274261+PedroAbreuHS@users.noreply.github.com>
2024-06-13 18:12:44 -0300

    Ajuste dependencias entre projetos

--- src/InvestmentPortfolio.Domain/Entities/EntityBase.cs
+++ src/InvestmentPortfolio.Domain/Entities/EntityBase.cs
@@ -4,5 +4,10 @@ namespace InvestmentPortfolio.Domain.Entities
     public class EntityBase
     {
         public Guid Id { get; set; }
+
+        protected EntityBase()
+        {
+            Guid Id = Guid.NewGuid();
+        }
     }
 }
